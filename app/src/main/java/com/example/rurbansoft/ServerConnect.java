8faf59e14c39fc1cb3dbce32e574edbd89060066
Riyanshi <riyanshiverma243@gmail.com>
2022-11-16 15:54:02 +0530

    server details updated.

--- app/src/main/java/com/example/rurbansoft/ServerConnect.java
+++ app/src/main/java/com/example/rurbansoft/ServerConnect.java
@@ -2,6 +2,6 @@ package com.example.rurbansoft;
 
 public class ServerConnect {
 
-    public static final String SERVER_URL_Users = "https://192.168.0.108/SqliteSync/saveUsers.php";
-    public static final String SERVER_URL_WorkItem="https://192.168.0.108/SqliteSync/saveWorkItem.php";
+    public static final String SERVER_URL_Users = "https://192.168.0.111/SqliteSync/saveUsers.php";
+    public static final String SERVER_URL_WorkItem="https://192.168.0.111/SqliteSync/saveWorkItem.php";
 }
